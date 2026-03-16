155ffcaf8827c1fe1699f077e2104c4d842aa566
goola971 <janviercharly97114@gmail.com>
2026-03-16 09:08:41 +0100

    save

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,12 +9,12 @@ function Header({ title }: { title: string }) {
 					<BellDot size={25} className="invert" />
 					<div className="absolute top-2.5 right-2 w-2.5 h-2.5 bg-red-500 rounded-full"></div>
 				</button>
-				<div className="flex items-center gap-2 cursor-pointer bg-[#8D8D8D] h-10 flex items-center justify-center rounded">
+				<div className="flex items-center gap-2 cursor-pointer bg-[#8D8D8D] h-10 flex items-center justify-center rounded px-2">
 					<Search size={25} className="invert" />
 					<input
 						type="text"
 						placeholder="Search"
-						className="outline-none text-white"
+						className="outline-none text-white bg-transparent w-full h-full placeholder:text-white"
 					/>
 				</div>
 				{/* user lucide react */}
