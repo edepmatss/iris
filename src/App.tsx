8bbbc88fb7de2f7674ed095f1b7fa1d43153cfb9
dacostatimeo <timeo.dacosta77860@gmail.com>
2026-03-19 08:54:17 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import Module1 from "./page/module1/module1";
 import Module2 from "./page/module2/module2";
 import Module3 from "./page/module3/module3";
 import Module4 from "./page/module4/module4";
-import Module5 from "./page/module5/module5";
 import Accueil from "./page/accueil/accueil";
 
 const GuestLayout = () => {
@@ -62,7 +61,6 @@ function App() {
 				<Route path="/module2" element={<Module2 />} />
 				<Route path="/module3" element={<Module3 />} />
 				<Route path="/module4" element={<Module4 />} />
-				<Route path="/module5" element={<Module5 />} />
 			</Route>
 
 			<Route element={<AdminLayout />}>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import logo from '../assets/Logo1.svg';
 import logoIcon from '../assets/logoIcon.svg';
 import diagram from '../assets/diagram.svg';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NAV_ITEMS_ADMIN = [
 	{
@@ -83,16 +83,7 @@ const NAV_ITEMS_USER = [
 				<path strokeLinecap="round" strokeLinejoin="round" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
 			</svg>
 		),
-	},
-	{
-		id: 'module5',
-		label: 'Module 5',
-		icon: (
-			<svg fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5} className="w-7 h-7">
-				<path strokeLinecap="round" strokeLinejoin="round" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
-			</svg>
-		),
-	},
+	}
 ];
 
 export default function Navbar() {
